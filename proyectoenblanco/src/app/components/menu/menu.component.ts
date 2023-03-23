ac0b1fb44176b9fa61fc7feea5b2a2bda34ee7ec
RonSanchezS <58291957+RonSanchezS@users.noreply.github.com>
2023-03-23 08:46:29 -0400

    Clase 23/03/2023, fetch de api

--- proyectoenblanco/src/app/components/menu/menu.component.ts
+++ proyectoenblanco/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {}
 
+  navegarAPagina(pagina : string){
+    //navegar a about
+    alert("Navegando a " + pagina);
+    this.router.navigate([`${pagina}`]);
+  }
+  
 }
